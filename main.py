238cb4f98748a0455dc9d1d906f64639b6b27910
Luc Mosser <luc.mosser86@orange.fr>
2020-11-13 15:39:18 +0100

    changing some settings

--- main.py
+++ main.py
@@ -3,12 +3,17 @@ import os
 
 # setup display
 pygame.init()
-win = WIDTH, HEIGHT = 800, 500
-pygame.display.set_mode((WIDTH, HEIGHT))
+WIDTH, HEIGHT = 800, 500
+win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Hangman game !")
 
 # load images
-images = 
+images = []
+for i in range(7):
+    image = pygame.image.load("hangman" + str(i) + ".png")
+    images.append(image)
+# game variables
+hangman_status = 0
 # setup game loop
 FPS = 60
 clock = pygame.time.Clock(FPS)
@@ -16,11 +21,14 @@ run = True
 
 while run:
     clock.tick(FPS)
-    
+
+    win.fill((255, 255, 255))
+    pygame.display.update()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-            pos = pygam.mouse.get_pos()
+            pos = pygame.mouse.get_pos()
             print(pos)
 pygame.quit()
